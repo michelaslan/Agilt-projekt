da22bc818a426b76f4757f3cad32e392556e083e
Shibowie <shibowie@gmail.com>
2026-03-24 13:06:15 +0100

    typ fixat, men ser inte antalet spelare/lag

--- app.js
+++ app.js
@@ -46,6 +46,27 @@ function renderPlayer(p, team) {
     return li
 }
 
+async function loadEuropeanCountries() {
+    const select = document.getElementById("country");
+
+    try {
+        const response = await fetch("https://restcountries.com/v3.1/region/europe?fields=name");
+        const countries = await response.json();
+
+        countries.sort((a, b) =>
+            a.name.common.localeCompare(b.name.common)
+        );
+
+        countries.forEach(country => {
+            const option = document.createElement("option");
+            option.value = country.name.common;
+            option.textContent = country.name.common;
+            select.appendChild(option);
+        });
+    }   catch (error) {
+        console.error("Fel vid hämtning av länder:", error);
+    }
+}
 
 function renderHome() {
     document.getElementById("teamAName").textContent = teamAName
@@ -209,7 +230,7 @@ function renderPlayerInfo() {
 
 function editPlayer(username) {
     let player = teamA.find(p => p.username === username) ||
-                 teamB.find(p => p.username === username);
+        teamB.find(p => p.username === username);
 
     const profile = document.getElementById("profile");
 
@@ -250,7 +271,6 @@ function editPlayer(username) {
     `;
 }
 
-
 function updatePlayer(originalUsername) {
 
     const newUsername = document.getElementById("editUsername").value;
@@ -275,4 +295,4 @@ function updatePlayer(originalUsername) {
 
     save();
     renderPlayerInfo();
-}
+} 
